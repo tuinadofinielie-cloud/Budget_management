e27435e1379d45d35d5c12ed8b80a6087b585d9c
Elie <tuinadofinielie@gmail.com>
2026-08-28 16:41:17 +0000

    fix(mobile): bump compileSdk to 37 for flutter_secure_storage compatibility

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.financeapp.finance_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
